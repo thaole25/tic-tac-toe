aa79f5fe42e52df021dc69d781e15b4e1ec80d85
thaole <thao.t.le25@gmail.com>
2018-11-06 20:42:22 +1100

    added tie game

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tic-tac-toe
+inspired by freeCodeCamp.org tutorial: https://www.youtube.com/watch?v=P2TcQ3h0ipQ&t=2209s
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let board = [];
+let emptyTiles = [];
 const numTiles = 9;
 const rowTiles = 3; 
 let saveMoves = {}; // history moves of each player
 
 const humanPlayer = 'X';
+const aiPlayer = 'O';
+let endGame = false;
 const cells = document.querySelectorAll('.cell');
 
 // Create the results of winning
@@ -35,13 +38,14 @@ for (let j = (rowTiles - 1); j < (numTiles - rowTiles + 1); j+= (rowTiles - 1)){
     diagonal2.push(j);
 }
 winList.push(diagonal2);
-console.log(winList);
+//console.log(winList);
 
 startGame();
 
 function startGame(){
     for (let i = 0; i < numTiles; i++){
         board.push(i);
+        emptyTiles.push(i);
     }
 
     for (let i = 0; i < cells.length; i++){
@@ -53,40 +57,56 @@ function startGame(){
 function makeMove(cell){
     const selectedCellId = cell.target.id;
     moveOfPlayer(selectedCellId, humanPlayer);
+    if (!endGame){
+        moveOfPlayer(emptyTiles[0], aiPlayer);
+    }
 }
 
 function moveOfPlayer(selectedCellId, player){
-    const selectedCell = document.getElementById(selectedCellId);
-    selectedCell.innerText = player;
-    
-    //Save history moves of player
-    if (!saveMoves[player]){
-        saveMoves[player] = [];
-    }
-    saveMoves[player].push(parseInt(selectedCellId));
-    
-    //Check if the player won
-    let isWon = false;
-    for (let i = 0; i < winList.length; i++){
-        isWon = winList[i].every(elem => saveMoves[player].includes(elem));
-        
-        // If the player won
-        if (isWon){
-            //const resetButton = document.getElementById('reset');
-            //resetButton.style.display = 
-            
-            for (let tileIndex of winList[i]){
-                document.getElementById(tileIndex).style.backgroundColor = "red";   
-            }
+     // Check tie
+    if (emptyTiles.length == 0 && !endGame){
+        document.getElementById("result").innerText = "Tie game";
+        endGame = true;
+        gameOver();
+    }else{
+        const selectedCell = document.getElementById(selectedCellId);
+        selectedCell.innerText = player;
+        const idx = emptyTiles.indexOf(parseInt(selectedCellId));
+        emptyTiles.splice(idx, 1);
 
-            for (let i = 0; i < numTiles; i++){
-                cells[i].removeEventListener('click', makeMove);
-            }
-            break;
-        }   
-    }
+        //Save history moves of player
+        if (!saveMoves[player]){
+            saveMoves[player] = [];
+        }
+        saveMoves[player].push(parseInt(selectedCellId));
     
-    // winList.some(
-    //     winElem => winElem.every(elem => saveMoves[player].includes(elem))
-    // );
+        //Check if the player won
+        for (let i = 0; i < winList.length; i++){
+            endGame = winList[i].every(elem => saveMoves[player].includes(elem));
+        
+            // If the player won
+            if (endGame){
+                let color = player == humanPlayer ? "blue": "red";
+                let announce = player == humanPlayer ? "You won": "You lose";
+                for (let tileIndex of winList[i]){
+                    document.getElementById(tileIndex).style.backgroundColor = color;
+                }
+                document.getElementById("result").innerText = announce;
+                gameOver();
+                break;
+            }   
+        }
+    }
+}
+
+function gameOver(){
+    for (let i = 0; i < cells.length; i++){
+        //cells[i].style.backgroundColor = "yellow";
+        cells[i].removeEventListener('click', makeMove);
+    }
+    const resultDiv = document.getElementById("result");
+    resultDiv.style.display = "block";
+
+    //const resetButton = document.getElementById('reset');
+    //resetButton.style.display = 
 }
